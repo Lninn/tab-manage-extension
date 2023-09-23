7e7fb88e2bc5f0c5ff6e2f0013a6e3ffdeb63ecc
Lninn <thl384458870@outlook.com>
2023-09-23 23:26:38 +0800

    update

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        background: 'src/background.js'
+      },
+      output: {
+        entryFileNames(chunkInfo) {
+          if (chunkInfo.name === 'background') {
+            return 'background.js'
+          } else {
+            return "assets/[name]-[hash].js"
+          }
+        },
+      }
+    }
+  }
 })
